9f602eddacc02605425768a77c13c45ecae2bbea
Vespertino <vespertino@max11>
2025-01-30 20:53:23 +0100

    Modelos terminados

--- entorno_odoo_docker/addons/clsu_brews_and_boards/models/models.py
+++ entorno_odoo_docker/addons/clsu_brews_and_boards/models/models.py
@@ -19,75 +19,238 @@
 
 # brand_id = fields.Many2one('sge_scooters.brand', string='Fabricante de patinetes')
 # clients_id =fields.One2many('sge_scooters.client','bike_id',string='Reservada por ')
+# pedido = fields.Many2many('clsu_brews_and_boards.pedido', 'tu_tabla_relacion', 'columna_id_actual', 'columna_id_pedido', string='Número de pedido')
 
 from odoo import models, fields, api # type: ignore
 class comida(models.Model):
     _name = 'clsu_brews_and_boards.comida'
     _description = 'Menú de comida'
 
-    id = fields.Char()
-    nombre = fields.Char()
-    precio = fields.Float()
-    tipo = fields.Selection(string='Tipo', selection=[('snacks','Picoteo'),('dish','Primer plato'),('dessert','Postre')])
+    name = fields.Char('Comidas')
+    id = fields.Char('Codigo')
+    nombre = fields.Char('Nombre de la comida')
+    precio = fields.Float('Precio')
+    tipo = fields.Selection(
+        string='Tipo', 
+        selection=[
+            ('snacks','Picoteo'),
+            ('dish','Primer plato'),
+            ('dessert','Postre')
+        ]
+    )
 
-    pedido = fields.Mani2one('clsu_brews_and_boards.pedido', string='Número de pedido')
+    pedido_id = fields.Many2many(
+        'clsu_brews_and_boards.pedido', #Modelo relacionado
+        'pedido_comida',                #Nombre de la tabla intermedia
+        'comida_id',                    #Referencia a este modelo
+        'pedido_id',                    #Referencia al otro modelo
+        string='Número de pedido'
+    )
 
 class bebida(models.Model):
     _name = 'clsu_brews_and_boards.bebida'
     _description = 'Menú de bebidas'
 
-    id = fields.Char()
-    nombre = fields.Char()
-    precio = fields.Float()
-    tamaño = fields.Selection(string='Tamaño', selection=[('small','Pequeño'),('meidum','Mediano'),('tall','Grande')])
+    name = fields.Char('Bebidas')
+    id = fields.Char('Codigo')
+    nombre = fields.Char('Nombre de la bebida')
+    precio = fields.Float('Precio')
+    tamaño = fields.Selection(
+        string='Tamaño', 
+        selection=[
+            ('small','Pequeño'),
+            ('medium','Mediano'),
+            ('tall','Grande')
+        ]
+    )
+
+    pedido_id = fields.Many2many(
+        'clsu_brews_and_boards.pedido', #Modelo relacionado
+        'pedido_bebida',                #Nombre de la tabla intermedia
+        'bebida_id',                    #Referencia a este modelo
+        'pedido_id',                    #Referencia al otro modelo
+        string='Número de pedido'
+    )
 
 class pedido(models.Model):
     _name = 'clsu_brews_and_boards.pedido'
     _description = 'Pedido de comida'
 
-    id = fields.Char()
-    hora = fields.Date()
+    name = fields.Char('Pedidos')
+    id = fields.Char('Codigo')
+    hora = fields.Datetime('Hora del pedido')
+
+    comida_id = fields.Many2many(
+        'clsu_brews_and_boards.comida',
+        'pedido_comida',
+        'pedido_id',
+        'comida_id',
+        string='Comida en el pedido'
+    )
+    bebida_id = fields.Many2many(
+        'clsu_brews_and_boards.bebida',
+        'pedido_bebida',
+        'pedido_id',
+        'bebida_id',
+        string='Bebida en el pedido'
+    )
+    staff_id=fields.Many2one(
+        'clsu_brews_and_boards.staff', 
+        string="Encargado del pedido"
+    )
+    jugador_id=fields.Many2one(
+        'clsu_brews_and_boards.jugador', 
+        string="Pedido para "
+    )
 
 class staff(models.Model):
     _name = 'clsu_brews_and_boards.staff'
     _description = 'Trabajadores'
 
-    id = fields.Char()
-    nombre = fields.Char()
-    telf = fields.Integer()
-    email = fields.Char()
-    rol = fields.Selection(string='Rol', selection=[('mess','Mesero'),('coff','Barista'),('mangr','Administrador')])
+    name = fields.Char('Staff')
+    id = fields.Char('Codigo')
+    nombre = fields.Char('Nombre: ')
+    telf = fields.Integer('Telefono: ')
+    email = fields.Char('Email: ')
+    rol = fields.Selection(
+        string='Rol', 
+        selection=[
+            ('mess','Mesero'),
+            ('coff','Barista'),
+            ('mangr','Administrador')
+        ]
+    )
+
+    pedido_id=fields.One2many(
+        'clsu_brews_and_boards.pedido', 
+        'staff_id', 
+        string='Pedidos realizados'
+    )
 
 class jugador(models.Model):
     _name = 'clsu_brews_and_boards.jugador'
     _description = 'Cliente habitual'
 
-    id = fields.Char()
-    nombre = fields.Char()
-    alias = fields.Char()
+    name = fields.Char('Jugadores')
+    id = fields.Char('Codigo: ')
+    nombre = fields.Char('Nombre completo: ')
+    alias = fields.Char('Alias: ')
+
+    pedido_id=fields.One2many(
+        'clsu_brews_and_boards.pedido', 
+        'jugador_id', 
+        string='Pedidos realizados'
+    )
+    torneo_id = fields.Many2many(
+        'clsu_brews_and_boards.torneo',
+        'torneo_jugador',
+        'torneo_id',
+        'jugador_id',
+        string='Participa en '
+    )
+    reserva_id=fields.One2many(
+        'clsu_brews_and_boards.reserva', 
+        'jugador_id', 
+        string='Pedidos realizados'
+    )
 
 class torneo(models.Model):
     _name = 'clsu_brews_and_boards.torneo'
     _description = 'Torneo de juego'
 
-    id = fields.Char()
-    nombre = fields.Char()
-    premio = fields.Char()
-    fecha = fields.Date()
+    name = fields.Char('Torneos')
+    id = fields.Char('Codigo')
+    nombre = fields.Char('Titulo del torneo: ')
+    premio = fields.Char('Premio del torneo: ')
+    fecha_inicio = fields.Date('Fecha establecida de inicio: ')
+    fecha_final = fields.Date('Fecha establecida de fin:')
+
+    jugador_id = fields.Many2many(
+        'clsu_brews_and_boards.jugador',
+        'torneo_jugador',
+        'jugador_id',
+        'torneo_id',
+        string='Participa '
+    )
+    juego_id=fields.Many2one(
+        'clsu_brews_and_boards.juego', 
+        string="Pedido para "
+    )
+
+class juego(models.Model):
+    _name = 'clsu_brews_and_boards.juego'
+    _description = 'Torneo de juego'
+
+    name = fields.Char('Juegos de mesa')
+    id = fields.Char('Codigo')
+    nombre = fields.Char('Nombre del juego: ')
+    genero = fields.Selection(
+        string='Género del juego', 
+        selection=[
+            ('fam','Familiar'),
+            ('party','Fiesta'),
+            ('strategy','Estrategia'),
+            ('coop','Cooperativos'),
+            ('skill','Destreza y habilidad'),
+            ('abstract','Abstractos'),
+            ('deduc','Deduccion y faroleo')
+        ]
+    )
+    num_jugadores = fields.Integer('Numero de jugadores recomendados')
+    duracion = fields.Integer('Duracion media de partida')
+
+    torneo_id=fields.One2many(
+        'clsu_brews_and_boards.torneo', 
+        'juego_id', 
+        string='Se juega en el torneo '
+    )
+    reserva_id=fields.One2many(
+        'clsu_brews_and_boards.reserva',
+        'juego_id',
+        string='Reservado a '
+    )
 
 class mesa(models.Model):
     _name = 'clsu_brews_and_boards.mesa'
     _description = 'Mesas del local'
 
-    num_mesa = fields.Char()
-    capacidad = fields.Integer()
+    name = fields.Char('Mesas disponibles')
+    num_mesa = fields.Char('Codigo')
+    capacidad = fields.Integer('Cupo de sillas: ')
     tipo = fields.Boolean(string='Apto para juego', default=True)
-    estado = fields.Selection(string='Estado', selection=[('free','Libre'),('stop','Ocupada'),('resr','Reservada')])
+    estado = fields.Selection(
+        string='Estado', 
+        selection=[
+            ('free','Libre'),
+            ('stop','Ocupada'),
+            ('resr','Reservada')
+        ]
+    )
+
+    reserva_id=fields.Many2one(
+        'clsu_brews_and_boards.reserva',
+        string='Mesa reservada: '
+    )
 
 class reserva(models.Model):
     _name = 'clsu_brews_and_boards.reserva'
     _description = 'Registro de reserva'
 
-    id = fields.Char()
-    hora = fields.Date()
-    jugadores = fields.Integer()
+    name = fields.Char('Reservas')
+    id = fields.Char('Codigo')
+    hora = fields.Datetime('Hora de la reserva: ')
+    jugadores = fields.Integer('Número de personas para la reserva: ')
+
+    jugador_id=fields.Many2one(
+        'clsu_brews_and_boards.jugador', 
+        string="Reservado para: "
+    )
+    juego_id=fields.Many2one(
+        'clsu_brews_and_boards.juego',
+        string='Juego reservado '
+    )
+    mesa_id=fields.One2many(
+        'clsu_brews_and_boards.mesa',
+        'reserva_id',
+        string='Reservado a '
+    )
